a82135bbd5d14b3434980a655253f99f68103f01
Brayden Houston <braydenjhouston@gmail.com>
2018-11-23 21:15:09 -0700

    Add to run word function

--- main.py
+++ main.py
@@ -4,28 +4,50 @@ import csv
 
 
 def readFile(filename):
-    obj = dict()
+    obj = {}
+    previous_from = ""
     with open(filename) as f:
         reader = csv.reader(f, delimiter=',')
         for row in reader:
             if not row[0][0] == '#':
                 # print(row)
                 try:
+                    if previous_from != row[0].strip() or not previous_from:
+                        obj[row[0].strip()] = {}
                     if row[1]:
-                        obj[row[0].strip()][row[0].strip()] = {'new_letter': row[2].strip(),
+                        obj[row[0].strip()][row[1].strip()] = {'new_letter': row[2].strip(),
                                                                'direction': row[3].strip(),
                                                                'to': row[4].strip()
-                                                              }
+                                                               }
                 except IndexError:
                     obj[row[0].strip()] = {'from': row[0].strip()}
+                previous_from = row[0].strip()
     return obj
 
 
 def runWord(obj, word):
+    print(obj)
     if not word:
         print("No word provided")
         return False
-    printHead = obj['1 Start']
+
+    state = '1 Start'
+    printHead = 0
+    word = list(word)
+
+    word[printHead] = obj[state][word[printHead]]["new_letter"]
+
+    if obj[state][word[printHead]]["direction"] == 'R':
+        printHead += 1
+    elif obj[state][word[printHead]]["direction"] == 'L':
+        printHead -= 1
+
+    if printHead < 0:
+        print("Print head moved before 0")
+        return False
+
+    print(word)
+
     return True
 
 
@@ -33,7 +55,7 @@ def main():
     obj = readFile('test.csv')
     # print(readFile(sys.argv[0]))
     # print(runWord(obj, sys.argv[1]))
-    testword = ""
+    testword = "abba"
     if runWord(obj, testword):
         print("Accepted: " + testword)
     else:
